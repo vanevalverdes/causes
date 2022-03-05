7832cca904dfaae4d66994af1e6cf21862e3da51
Vanessa C. Valverde <vane.valverdesantos@gmail.com>
2022-03-04 22:33:30 -0300

    Imprime Country

--- index.php
+++ index.php
@@ -5,14 +5,13 @@ $ipaddress = getenv("REMOTE_ADDR") ;
 Echo "Your IP Address is " . $ipaddress;
 $cURLConnection = curl_init();
 
-curl_setopt($cURLConnection, CURLOPT_URL, 'http://ip-api.com/json/'. $ipaddress 'fields=country');
+curl_setopt($cURLConnection, CURLOPT_URL, 'http://ip-api.com/json/'. $ipaddress .'?fields=country');
 curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 
-$phoneList = curl_exec($cURLConnection);
+$list = curl_exec($cURLConnection);
 curl_close($cURLConnection);
 
-$json = json_decode($phoneList, true);
-var_dump($json);
-
-
+$json = json_decode($list, true);
+$country = $json['country'];
+var_dump($country)
 ?>
